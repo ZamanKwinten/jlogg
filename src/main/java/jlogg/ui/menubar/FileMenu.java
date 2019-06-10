9adc584da1f9d5c459547c6520e610839b24964d
Kwinten <42929018+ZamanKwinten@users.noreply.github.com>
2019-06-10 11:34:45 +0200

    Search: Added saving search results
    - Made it possible to save the results of a search as a new file 
    - Automatically open the newly created results fiel

--- src/main/java/jlogg/ui/menubar/FileMenu.java
+++ src/main/java/jlogg/ui/menubar/FileMenu.java
@@ -1,7 +1,12 @@
 package jlogg.ui.menubar;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -12,14 +17,19 @@ import javafx.scene.input.KeyCombination;
 import javafx.stage.FileChooser;
 import jlogg.eventbus.EventBusFactory;
 import jlogg.eventbus.IndexStartEvent;
+import jlogg.ui.GlobalConstants;
 import jlogg.ui.MainPane;
 
 public class FileMenu extends Menu {
+	private final Logger logger = Logger.getLogger(FileMenu.class.getName());
+
+	private File previousDir;
 
 	private final MenuItem openMenuItem;
 	private final MenuItem closeMenuItem;
 	private final MenuItem closeAllMenuItem;
 	private final MenuItem exitMenuItem;
+	private final MenuItem saveAsMenuItem;
 
 	public FileMenu(MainPane mainPane) {
 		super("File");
@@ -28,6 +38,7 @@ public class FileMenu extends Menu {
 		openMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
 		openMenuItem.setOnAction((event) -> {
 			FileChooser fc = new FileChooser();
+			fc.setInitialDirectory(previousDir);
 			fc.setTitle("Open log file");
 			List<File> files = fc.showOpenMultipleDialog(null);
 			if (files != null) {
@@ -35,6 +46,8 @@ public class FileMenu extends Menu {
 				for (File file : files) {
 					mainPane.addTab(file);
 					EventBusFactory.getInstance().getEventBus().post(new IndexStartEvent(file));
+
+					previousDir = file.getParentFile();
 				}
 			}
 		});
@@ -44,10 +57,39 @@ public class FileMenu extends Menu {
 
 		closeAllMenuItem = new MenuItem("Close All");
 
+		saveAsMenuItem = new MenuItem("Save Search Results...");
+		saveAsMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
+		saveAsMenuItem.setOnAction((event) -> {
+			// File must be opened + search must have results
+			if (mainPane.getCurrentSelectedTab() != null && !GlobalConstants.searchResults.isEmpty()) {
+				FileChooser fc = new FileChooser();
+				// Probably the most logical thing to try & save it in the same dir as the
+				// parent file?
+				fc.setInitialDirectory(mainPane.getCurrentSelectedTab().getFile().getParentFile());
+				fc.setInitialFileName("jlogg - " + System.currentTimeMillis() + ".log");
+				File file = fc.showSaveDialog(null);
+				if (file != null) {
+					previousDir = file.getParentFile();
+					try {
+						Files.write(Paths.get(file.getAbsolutePath()), ((Iterable<String>) GlobalConstants.searchResults
+								.stream().map(logline -> logline.getLineString())::iterator));
+
+						// File was successfully written => open it
+						mainPane.addTab(file);
+						EventBusFactory.getInstance().getEventBus().post(new IndexStartEvent(file));
+
+					} catch (IOException e) {
+						logger.log(Level.SEVERE, "Error during saving", e);
+					}
+				}
+			}
+		});
+
 		exitMenuItem = new MenuItem("Exit");
 		exitMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN));
 
-		getItems().addAll(openMenuItem, closeMenuItem, closeAllMenuItem, new SeparatorMenuItem(), exitMenuItem);
+		getItems().addAll(openMenuItem, closeMenuItem, closeAllMenuItem, new SeparatorMenuItem(), saveAsMenuItem,
+				new SeparatorMenuItem(), exitMenuItem);
 	}
 
 }
