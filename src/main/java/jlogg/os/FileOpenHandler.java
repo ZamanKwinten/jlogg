28bd141158fed8505fbeb6958e6363d9bfee8ff7
KwintenZaman <kwinten.zaman@gmail.com>
2021-02-21 23:43:25 +0100

    Fix for null pointer exception on Mac

--- src/main/java/jlogg/os/FileOpenHandler.java
+++ src/main/java/jlogg/os/FileOpenHandler.java
@@ -1,13 +1,14 @@
 package jlogg.os;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Platform;
 import jlogg.ui.MainStage;
 
 public abstract class FileOpenHandler {
-	public static List<File> initialFiles;
+	public static List<File> initialFiles = new ArrayList<>();
 
 	protected final MainStage stage;
 
