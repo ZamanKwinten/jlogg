b65fe28cfc57f3274d8dbbfc1e2714a13056576d
KwintenZaman <kwinten.zaman@gmail.com>
2021-05-27 22:59:26 +0200

    Make it possible for plugin popups to use the same styling as the main application

---
+++ src/main/java/jlogg/ui/utils/StyledScene.java
@@ -0,0 +1,16 @@
+package jlogg.ui.utils;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import jlogg.ui.GlobalConstants;
+import jlogg.ui.css.ResourceLoader;
+
+public class StyledScene extends Scene {
+
+	public StyledScene(Parent root) {
+		super(root);
+		getStylesheets().addAll(ResourceLoader.loadResourceFile("color.css"));
+		getRoot().setStyle("-fx-base:" + GlobalConstants.theme.getValue().getFXBase());
+	}
+
+}
