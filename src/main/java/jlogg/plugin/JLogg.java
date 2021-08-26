8e00d0d35678003ad33ce54ade89a005b11828e4
KwintenZaman <kwinten.zaman@gmail.com>
2021-08-27 00:03:37 +0200

    Make it possible for plugins to do searches on all files in one go

--- src/main/java/jlogg/plugin/JLogg.java
+++ src/main/java/jlogg/plugin/JLogg.java
@@ -1,11 +1,17 @@
 package jlogg.plugin;
 
+import java.io.File;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import jlogg.datahandlers.FileLineReader;
 import jlogg.datahandlers.PluginFileIterator;
+import jlogg.ui.FileTab;
 import jlogg.ui.MainPane;
 import jlogg.ui.MainStage;
+import jlogg.ui.popup.SearchPopup;
 
 public class JLogg {
 	private static PluginFileIterator iterator;
@@ -18,6 +24,28 @@ public class JLogg {
 		iterator = new PluginFileIterator(MainStage.getInstance().getMainPane().getCurrentSelectedTab().getFile(),
 				action);
 		iterator.doIt();
+	}
+
+	public static void doItOnAllFiles(PluginAction action) {
+
+		MainPane mainPane = MainStage.getInstance().getMainPane();
+		List<FileTab> fileTabs = mainPane.getFileTabs();
+		if (fileTabs.size() == 1) {
+			doItOnCurrentFile(action);
+		} else if (fileTabs.size() > 1) {
+			SearchPopup popup = new SearchPopup(
+					fileTabs.stream().map(fileTab -> fileTab.getFile()).collect(Collectors.toList()));
+			Optional<List<File>> result = popup.open();
+
+			if (result.isPresent()) {
+				if (iterator != null) {
+					iterator.stop();
+				}
+
+				iterator = new PluginFileIterator(result.get(), action);
+				iterator.doIt();
+			}
+		}
 	}
 
 	public static String readLine(LogLine line) {
