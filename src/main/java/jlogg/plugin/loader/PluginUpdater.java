e23aea0790d784d0affb37d10b858f1b721ddad6
Kwinten Zaman <42929018+ZamanKwinten@users.noreply.github.com>
2024-07-31 15:28:54 +0200

    Plugin update make sure to  only update the plugin if needed

--- src/main/java/jlogg/plugin/loader/PluginUpdater.java
+++ src/main/java/jlogg/plugin/loader/PluginUpdater.java
@@ -17,6 +17,8 @@ import java.util.logging.Logger;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import jlogg.version.VersionUtil;
+
 public class PluginUpdater {
 	private static final Logger logger = Logger.getLogger(PluginUpdater.class.getName());
 	private static final Gson gson = new GsonBuilder().create();
@@ -24,10 +26,10 @@ public class PluginUpdater {
 	private static final long UPDATE_CHECK_DELTA = 24 * 60 * 60 * 1000; // try once a day
 
 	public static void tryUpdate(PluginManifestData manifestData, File pluginLocation) {
-		File lastSuccess = new File(pluginLocation.getParentFile(), pluginLocation.getName() + LAST_TRY_EXTENSION);
-		if (lastSuccess.exists() && System.currentTimeMillis() - lastSuccess.lastModified() < UPDATE_CHECK_DELTA) {
+		if (recentPluginUpdate(pluginLocation)) {
 			return;
 		}
+
 		try {
 			var pluginServerURL = manifestData.serverURL().get();
 
@@ -37,6 +39,15 @@ public class PluginUpdater {
 			}
 
 			var response = optResponse.get();
+			var manifest = response.manifest();
+
+			if (VersionUtil.isLaterJLoggVersion(manifest.jloggVersion())
+					|| !VersionUtil.isLaterVersion(manifestData.jloggPluginVersion(), manifest.jloggPluginVersion())) {
+				// No need to update since either the plugin is not runnable on the current
+				// JLogg version OR the version of the plugin is not later than the current one
+				updatePluginLock(pluginLocation);
+				return;
+			}
 
 			var downloadStream = sendGETRequest(
 					PluginServerEndPoints.constructDownloadURI(pluginServerURL, response.filename()),
@@ -45,16 +56,29 @@ public class PluginUpdater {
 			if (downloadStream.statusCode() == 200) {
 				Files.copy(downloadStream.body(), pluginLocation.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			}
-
-			if (!lastSuccess.exists()) {
-				lastSuccess.createNewFile();
-			} else {
-				lastSuccess.setLastModified(System.currentTimeMillis());
-			}
+			updatePluginLock(pluginLocation);
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Unable to update plugin: " + pluginLocation.getName(), e);
 		}
 
+	}
+
+	private static boolean recentPluginUpdate(File pluginLocation) {
+		File lastSuccess = getPluginUpdateLock(pluginLocation);
+		return lastSuccess.exists() && System.currentTimeMillis() - lastSuccess.lastModified() < UPDATE_CHECK_DELTA;
+	}
+
+	private static void updatePluginLock(File pluginLocation) throws IOException {
+		File lockFile = getPluginUpdateLock(pluginLocation);
+		if (!lockFile.exists()) {
+			lockFile.createNewFile();
+		} else {
+			lockFile.setLastModified(System.currentTimeMillis());
+		}
+	}
+
+	private static File getPluginUpdateLock(File pluginLocation) {
+		return new File(pluginLocation.getParentFile(), pluginLocation.getName() + LAST_TRY_EXTENSION);
 	}
 
 	private static Optional<PluginServerLatestResponse> getLatestPluginData(String serverURL)
